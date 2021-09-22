9456b276ec900f69a305dd6e3c48b96d6eae321f
Sean Delaney <seandelaney42@gmail.com>
2021-09-21 22:06:20 -0400

    CSS Finished
    Finished CSS, starting work on JS.

---
+++ client/lab_4/script.js
@@ -0,0 +1,8 @@
+let slidePosition=0;
+const slides=document.querySelectorAll('carousel_item');
+const totalSlides=slides.length;
+console.log(slides);
+document.querySelector('carousel_button--next')
+.addEventListener('click',function(){
+    console.log('hello')
+})
