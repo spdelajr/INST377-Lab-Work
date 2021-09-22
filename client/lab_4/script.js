fa1a8619c9c10919bd89b0ef322174f495900214
Sean Delaney <seandelaney42@gmail.com>
2021-09-21 23:35:01 -0400

    Updated JS
    All the necessary JS should be in, I am getting a weird JS error on the console, but I am going to try to figure that out tomorrow. I just wanted to make sure I had all the necessary code.

--- client/lab_4/script.js
+++ client/lab_4/script.js
@@ -1,8 +1,46 @@
 let slidePosition=0;
 const slides=document.querySelectorAll('carousel_item');
 const totalSlides=slides.length;
-console.log(slides);
+
 document.querySelector('carousel_button--next')
-.addEventListener('click',function(){
-    console.log('hello')
-})
+.addEventListener("click",function(){
+    console.log("hello");
+    moveToNextSlide();
+});
+document.querySelector('carousel_button--prev')
+.addEventListener("click",function(){
+    console.log("hello");
+    moveToPrevSlide();
+});
+
+function updateSlidePosition(){
+    for (let slide of slides){
+        slide.classList.remove('carousel_item--visible');
+        slide.classList.add('carousel_item--hidden');
+    }
+    slides[slidePosition].classList.add('carousel_item--visible');
+
+}
+
+
+function moveToNextSlide() {
+    
+    console.log('hello next')
+    if(slidePosition === totalSlides-1){
+        slidePosition=0;
+    } else{
+        slidePosition++;
+    }
+    updateSlidePosition();
+}
+
+function moveToPrevSlide() {
+    
+    console.log('hello prev')
+    if(slidePosition === 0){
+        slidePosition=totalSlides-1;
+    } else{
+        slidePosition--;
+    }
+    updateSlidePosition();
+}
