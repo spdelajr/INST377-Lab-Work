ce8a8dfbca651731d7e1f7c45b899f4fe626c10a
Sean Delaney <seandelaney42@gmail.com>
2021-09-22 14:46:22 -0400

    Simplified script
    Tried to simplify my script to find the problem.

--- client/lab_4/script.js
+++ client/lab_4/script.js
@@ -1,24 +1,27 @@
 let slidePosition=0;
-const slides=document.querySelectorAll('carousel_item');
+const slides=document.querySelectorAll('.carousel_item');
 const totalSlides=slides.length;
+console.log(totalSlides);
 
-document.querySelector('carousel_button--next')
-.addEventListener("click",function(){
+const next =document.querySelector('.carousel_button--next');
+console.log('next button', next);
+next.addEventListener("click",function(){
     console.log("hello");
     moveToNextSlide();
 });
-document.querySelector('carousel_button--prev')
-.addEventListener("click",function(){
+const prev=document.querySelector('.carousel_button--prev');
+console.log('previous button', prev);
+prev.addEventListener("click",function(){
     console.log("hello");
     moveToPrevSlide();
 });
 
 function updateSlidePosition(){
     for (let slide of slides){
-        slide.classList.remove('carousel_item--visible');
-        slide.classList.add('carousel_item--hidden');
+        slide.classList.remove('.carousel_item--visible');
+        slide.classList.add('.carousel_item--hidden');
     }
-    slides[slidePosition].classList.add('carousel_item--visible');
+    slides[slidePosition].classList.add('.carousel_item--visible');
 
 }
 
