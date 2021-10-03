f5093f085eb8d7d16f4c2d0d35de77098b6925ca
Sean Delaney <seandelaney42@gmail.com>
2021-10-03 13:06:16 -0400

    Added Tetrominos
    All tetrominos were added and you can actually draw them on the grid.

--- client/tetris/script.js
+++ client/tetris/script.js
@@ -5,4 +5,52 @@ document.addEventListener('DOMContentLoaded',() => {
   const StartBtn = document.querySelector('#start-button')
   const width = 10
   console.log(squares)
+
+  const lTetromino = [
+    [1, width+1, width*2+1, 2],
+    [width, width+1, width+2, width*2+2],
+    [1, width+1, width*2+1, width*2],
+    [width, width*2, width*2+1, width*2+2]
+  ]
+  
+  const zTetromino = [
+    [0, width, width + 1, width * 2 + 1],
+    [width + 1, width + 2, width * 2, width * 2 + 1],
+    [0, width, width + 1, width * 2 + 1],
+    [width + 1, width + 2, width * 2, width * 2 + 1]
+  ]
+
+  const tTetromino = [
+    [1, width, width + 1, width + 2],
+    [1, width + 1, width + 2, width * 2 + 1],
+    [width, width + 1, width + 2, width * 2 + 1],
+    [1, width, width + 1, width * 2 + 1]
+  ]
+
+  const oTetromino = [
+    [0, 1, width, width + 1],
+    [0, 1, width, width + 1],
+    [0, 1, width, width + 1],
+    [0, 1, width, width + 1]
+  ]
+
+  const iTetromino = [
+    [1, width + 1, width * 2 + 1, width * 3 + 1],
+    [width, width + 1, width + 2, width + 3],
+    [1, width + 1, width * 2 + 1, width * 3 + 1],
+    [width, width + 1, width + 2, width + 3]
+  ]
+
+  const theTetrominoes = [lTetromino, zTetromino, tTetromino, oTetromino, iTetromino]
+
+  let currentPosition = 4
+  let current = theTetrominoes[0][0]
+  
+  function draw() {
+    current.forEach(index => {
+      squares[currentPosition+index].classList.add('tetromino')
+    })
+  }
+
+  draw()
 })
