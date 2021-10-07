01f318a4ed61f28304fc1ae9ba8708a4400f5af7
Sean Delaney <seandelaney42@gmail.com>
2021-10-06 20:15:52 -0400

    findMatches function
    findMatches implemented and working.

--- client/lab_6/script.js
+++ client/lab_6/script.js
@@ -2,4 +2,10 @@ const endpoint = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json
 const cities = [];
 fetch(endpoint)
   .then((blob) => blob.json())
-  .then((data) => { console.log(data); });
+  .then((data) => { cities.push(...data); });
+function findMatches(wordToMatch, cities) {
+  return cities.filter(place => {
+    const regex = new RegExp(wordToMatch, 'gi');
+    return place.name.match(regex);
+  });
+}
