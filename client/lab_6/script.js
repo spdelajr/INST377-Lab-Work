9fb019e3a3943c0d8fee73690c85b0d86f7f373e
Sean Delaney <seandelaney42@gmail.com>
2021-10-06 21:53:48 -0400

    Matches logged, no output
    Got displaymatches and html to function but it doesn't actually output the results in the list.

--- client/lab_6/script.js
+++ client/lab_6/script.js
@@ -1,11 +1,30 @@
 const endpoint = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json';
 const cities = [];
+const searchInput = document.querySelector('.search');
+const suggestions = document.querySelector('.suggestions');
 fetch(endpoint)
   .then((blob) => blob.json())
   .then((data) => { cities.push(...data); });
 function findMatches(wordToMatch, cities) {
-  return cities.filter(place => {
+  return cities.filter((place) => {
     const regex = new RegExp(wordToMatch, 'gi');
     return place.name.match(regex);
   });
 }
+
+function displayMatches() {
+  const matchArray = findMatches(this.value, cities);
+  console.log(matchArray);
+  const html = matchArray.map((place) => {
+    return `
+    <li>
+      <span class = 'name'>${place.name}, ${place.city}, ${place.state}</span>
+    </li>
+    `;
+  }).join('');
+  console.log(html)
+  suggestions.innerHtml = html;
+}
+
+searchInput.addEventListener('change', displayMatches);
+searchInput.addEventListener('keyup', displayMatches);
