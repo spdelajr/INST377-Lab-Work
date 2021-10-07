f91cea877076f73023520c0af6e5706350de60d5
Sean Delaney <seandelaney42@gmail.com>
2021-10-07 12:44:29 -0400

    Final Touches
    Followed the instructions after the tutorial.

--- client/lab_6/script.js
+++ client/lab_6/script.js
@@ -1,30 +1,34 @@
-const endpoint = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json';
-const cities = [];
-const searchInput = document.querySelector('.search');
-const suggestions = document.querySelector('.suggestions');
-fetch(endpoint)
-  .then((blob) => blob.json())
-  .then((data) => { cities.push(...data); });
-function findMatches(wordToMatch, cities) {
-  return cities.filter((place) => {
-    const regex = new RegExp(wordToMatch, 'gi');
-    return place.name.match(regex);
-  });
-}
+async function windowActions() {
+  const endpoint = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json';
+  const searchInput = document.querySelector('.search');
+  const suggestions = document.querySelector('.suggestions');
+  const request = await fetch(endpoint);
+  const cities = await request.json();
+  /* fetch(endpoint)
+    .then((blob) => blob.json())
+    .then((data) => { cities.push(...data); }); */
+  function findMatches(wordToMatch, cities) {
+    return cities.filter((place) => {
+      const regex = new RegExp(wordToMatch, 'gi');
+      return place.name.match(regex);
+    });
+  }
 
-function displayMatches() {
-  const matchArray = findMatches(this.value, cities);
-  console.log(matchArray);
-  const html = matchArray.map((place) => {
-    return `
+  function displayMatches(event) {
+    const matchArray = findMatches(event.target.value, cities);
+    console.log(matchArray);
+    const html = matchArray.map((place) => {
+      return `
     <li>
       <span class = 'name'>${place.name}, ${place.city}, ${place.state}</span>
     </li>
     `;
-  }).join('');
-  console.log(html);
-  suggestions.innerHTML = html;
-}
+    }).join('');
+    console.log(html);
+    suggestions.innerHTML = html;
+  }
 
-searchInput.addEventListener('change', displayMatches);
-searchInput.addEventListener('keyup', displayMatches);
+  searchInput.addEventListener('change', (evt) => { displayMatches(evt); });
+  searchInput.addEventListener('keyup', (evt) => { displayMatches(evt); });
+}
+window.onload = windowActions;
