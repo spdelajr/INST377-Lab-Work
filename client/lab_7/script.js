600122b92463a992ad62ce153f494d722e8fb55f
Sean Delaney <seandelaney42@gmail.com>
2021-10-10 13:47:10 -0400

    Final Five
    Updated JS so that it only includes first 5 restaurants that match the zip code.

--- client/lab_7/script.js
+++ client/lab_7/script.js
@@ -10,35 +10,35 @@ async function windowActions() {
   function findMatches(wordToMatch, cities) {
     return cities.filter((place) => {
       const regex = new RegExp(wordToMatch, 'gi');
-      return place.name.match(regex) || place.city.match(regex);
+      return place.zip.match(regex);
     });
   }
 
   function displayMatches(event) {
     const matchArray = findMatches(event.target.value, cities);
-    console.log(matchArray);
-    const html = matchArray.map((place) => {
+    const finalFive = matchArray.slice(0,5);
+    console.log(finalFive);
+    const html = finalFive.map((place) => {
       return `
           <li>
             <span class = 'name'>${place.name} <br/>${place.category} <em><br/>${place.address_line_1} <br/>${place.city} <br/>${place.zip}</em></span>
           </li>
         `;
     }).join('');
-    console.log(html);
     suggestions.innerHTML = html;
   }
 
   searchInput.addEventListener('change', (evt) => { displayMatches(evt); });
   searchInput.addEventListener('keyup', (evt) => { displayMatches(evt); });
 
-  let mymap = L.map('mapid').setView([51.505, -0.09], 13);
+  let mymap = L.map('mapid').setView([38.98, -76.93], 13);
   L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     id: 'mapbox/streets-v11',
     tileSize: 512,
     zoomOffset: -1,
-    accessToken: 'your.mapbox.access.token'
+    accessToken: 'pk.eyJ1Ijoic3BkZWxhanIiLCJhIjoiY2t1bGZuaXltMDBiZzJ1bGN1a2JtZGVnOSJ9.ZFtHmyCnSOxOXYtCbYoKzg'
 }).addTo(mymap);
 }
 window.onload = windowActions;
