96e9cd758f0b39786ec9758d3ec11d9d054ccce1
Sean Delaney <seandelaney42@gmail.com>
2021-10-10 11:31:51 -0400

    Copied my Lab 6 Code
    Lab 6 code copied in order to start lab 7.

---
+++ client/lab_7/script.js
@@ -0,0 +1,34 @@
+async function windowActions() {
+  const endpoint = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json';
+  const searchInput = document.querySelector('.search');
+  const suggestions = document.querySelector('.suggestions');
+  const request = await fetch(endpoint);
+  const cities = await request.json();
+  /* fetch(endpoint)
+      .then((blob) => blob.json())
+      .then((data) => { cities.push(...data); }); */
+  function findMatches(wordToMatch, cities) {
+    return cities.filter((place) => {
+      const regex = new RegExp(wordToMatch, 'gi');
+      return place.name.match(regex) || place.city.match(regex);
+    });
+  }
+
+  function displayMatches(event) {
+    const matchArray = findMatches(event.target.value, cities);
+    console.log(matchArray);
+    const html = matchArray.map((place) => {
+      return `
+          <li>
+            <span class = 'name'>${place.name} <br/>${place.category} <em><br/>${place.address_line_1} <br/>${place.city} <br/>${place.zip}</em></span>
+          </li>
+        `;
+    }).join('');
+    console.log(html);
+    suggestions.innerHTML = html;
+  }
+
+  searchInput.addEventListener('change', (evt) => { displayMatches(evt); });
+  searchInput.addEventListener('keyup', (evt) => { displayMatches(evt); });
+}
+window.onload = windowActions;
