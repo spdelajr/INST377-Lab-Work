7dc7c49b7021771402a32e84059d08330c6567f9
Sean Delaney <seandelaney42@gmail.com>
2021-10-10 14:47:48 -0400

    Map Markers
    I just added some map markers for all of the points. Some touch ups need to be done but overall it's working.

--- client/lab_7/script.js
+++ client/lab_7/script.js
@@ -4,6 +4,8 @@ async function windowActions() {
   const suggestions = document.querySelector('.suggestions');
   const request = await fetch(endpoint);
   const cities = await request.json();
+  let mapX = 0;
+  let mapY = 0;
   /* fetch(endpoint)
       .then((blob) => blob.json())
       .then((data) => { cities.push(...data); }); */
@@ -16,7 +18,7 @@ async function windowActions() {
 
   function displayMatches(event) {
     const matchArray = findMatches(event.target.value, cities);
-    const finalFive = matchArray.slice(0,5);
+    const finalFive = matchArray.slice(0, 5);
     console.log(finalFive);
     const html = finalFive.map((place) => {
       return `
@@ -28,17 +30,31 @@ async function windowActions() {
     suggestions.innerHTML = html;
   }
 
+  function displayMap(event) {
+    const matchArray = findMatches(event.target.value, cities);
+    const finalFive = matchArray.slice(0, 5);
+    console.log(finalFive);
+    mapX = finalFive[0].geocoded_column_1.coordinates[1];
+    mapY = finalFive[0].geocoded_column_1.coordinates[0];
+    let mymap = L.map('mapid').setView([mapX, mapY], 13);
+    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+      attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+      maxZoom: 18,
+      id: 'mapbox/streets-v11',
+      tileSize: 512,
+      zoomOffset: -1,
+      accessToken: 'pk.eyJ1Ijoic3BkZWxhanIiLCJhIjoiY2t1bGZuaXltMDBiZzJ1bGN1a2JtZGVnOSJ9.ZFtHmyCnSOxOXYtCbYoKzg'
+    }).addTo(mymap);
+    const marker1 = L.marker([mapX, mapY]).addTo(mymap);
+    const marker2 = L.marker([finalFive[1].geocoded_column_1.coordinates[1], finalFive[1].geocoded_column_1.coordinates[0]]).addTo(mymap);
+    const marker3 = L.marker([finalFive[2].geocoded_column_1.coordinates[1], finalFive[2].geocoded_column_1.coordinates[0]]).addTo(mymap);
+    const marker4 = L.marker([finalFive[3].geocoded_column_1.coordinates[1], finalFive[3].geocoded_column_1.coordinates[0]]).addTo(mymap);
+    const marker5 = L.marker([finalFive[4].geocoded_column_1.coordinates[1], finalFive[4].geocoded_column_1.coordinates[0]]).addTo(mymap);
+  }
+
   searchInput.addEventListener('change', (evt) => { displayMatches(evt); });
   searchInput.addEventListener('keyup', (evt) => { displayMatches(evt); });
-
-  let mymap = L.map('mapid').setView([38.98, -76.93], 13);
-  L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    id: 'mapbox/streets-v11',
-    tileSize: 512,
-    zoomOffset: -1,
-    accessToken: 'pk.eyJ1Ijoic3BkZWxhanIiLCJhIjoiY2t1bGZuaXltMDBiZzJ1bGN1a2JtZGVnOSJ9.ZFtHmyCnSOxOXYtCbYoKzg'
-}).addTo(mymap);
+  searchInput.addEventListener('change', (evt) => { displayMap(evt); });
+  searchInput.addEventListener('keyup', (evt) => { displayMap(evt); });
 }
 window.onload = windowActions;
